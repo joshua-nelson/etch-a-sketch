9ac5e0778313357a77f9a88183d3360ee1be31a3
joshua-nelson <28989859+joshua-nelson@users.noreply.github.com>
2023-02-07 09:23:47 -0600

    Add reset button functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.etch__main');
-
+const resetBtn = document.querySelector('.reset');
 
 function createGrid() {
     for (i = 1; i <= 256; i ++) {
@@ -8,13 +8,27 @@ function createGrid() {
         container.appendChild(grid);
     }
 }
+function reset() {
+    console.log(grid);
+    grid.forEach(e => {
+        if (e.classList.contains('active')) {
+            e.classList.remove('active');
+        }
+        
+    } );
+}
+
 createGrid();
- 
 
 const grid = Array.from(document.querySelectorAll('div.grid'));
-console.log(grid);
 grid.forEach(element => {
     element.addEventListener('mouseenter', function(e) {
         e.target.classList.add('active');
     });
 });
+
+resetBtn.addEventListener('click', function(){
+    reset();
+});
+
+
